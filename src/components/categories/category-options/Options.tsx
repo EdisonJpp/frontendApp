c21819cce4ec4adb7c9c508ebb56ebb771f51673
Edison Padilla <padilla.e@dhn.com.do>
2020-09-15 16:59:14 -0400

    width popover fixed

--- src/components/categories/category-options/Options.tsx
+++ src/components/categories/category-options/Options.tsx
@@ -2,7 +2,7 @@ import React from "react" ;
 
 export default  function  Opctions(){
     return(
-        <div  style={{width : "330px"}}>
+        <div className="popover-content">
              <span> Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las indus
                  trias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los m
                  ezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales c
